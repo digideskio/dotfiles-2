f45cc19c4afde3ef5755279c863d113d4a865169
jcrafford <jcrafford@gmail.com>
2013-05-09 12:19:13 -0300

    Update brew.sh

--- config/brew.sh
+++ config/brew.sh
@@ -76,6 +76,18 @@ brew install npm
 #brew tap josegonzalez/homebrew-php
 #brew install php54
 
+########################################
+# mysql
+# To connect:
+#     mysql -uroot
+# To have launchd start mysql at login:
+#     ln -sfv /usr/local/opt/mysql/*.plist ~/Library/LaunchAgents
+# Then to load mysql now:
+#     launchctl load ~/Library/LaunchAgents/homebrew.mxcl.mysql.plist
+# Or, if you don't want/need launchctl, you can just run:
+#     mysql.server start
+brew install mysql
+
 ###############################################################################
 # Native Apps
 ###############################################################################
